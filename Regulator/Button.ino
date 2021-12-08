e83172ea04c5ac45e2137c36ce8786255ff1b5e5
stealth-ultd <47171697+stealth-ultd@users.noreply.github.com>
2021-12-08 23:48:42 +0100

    Update Button.ino

--- Regulator/Button.ino
+++ Regulator/Button.ino
@@ -1,18 +1,19 @@
+// ** The button to enable/disable nighCharge and activate display during night **
 
 void buttonSetup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+pinMode(BUTTON_PIN, INPUT);
 }
 
 void buttonLoop() {
 
   const int LONG_PRESS_INTERVAL = 5000; // 5 sec
-  const int TOO_LONG_PRESS_INTERVAL = 2 * LONG_PRESS_INTERVAL;
+  const int RESET_PRESS_INTERVAL = 2 * LONG_PRESS_INTERVAL;
   static unsigned long pressStartMillis = 0;
   static boolean longPressDone = false;
 
-  boolean pressed = (digitalRead(BUTTON_PIN) == LOW);
+  boolean pressed = (digitalRead(BUTTON_PIN) == HIGH);
 
-  if (!buttonPressed) { // could be set from Blynk, Web, Telnet
+  if (!buttonPressed) { 
     buttonPressed = pressed; // set the global 'virtual' button press
   }
 
@@ -26,12 +27,13 @@ void buttonLoop() {
      longPressDone = false;
   } else { // counting
     unsigned long d = loopStartMillis - pressStartMillis;
-    if (d > TOO_LONG_PRESS_INTERVAL) { // button sticked?
-      heatingPower = 0;
-      alarmSound();
+    if (d > RESET_PRESS_INTERVAL) { // resetting button state
+      nightChargeDisable(); // disable nightCharge
+      resetSound();
+    
     } else if (!longPressDone && d > LONG_PRESS_INTERVAL) { // long press detected
       beep();
-      manualRunRequest = !manualRunRequest;
+      nightChargeInitiate(); // initiate nightCharge
       longPressDone = true;
     }
   }
