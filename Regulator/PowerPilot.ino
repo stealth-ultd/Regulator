edf38a18c031405ed57350e9932fc98585e7ba7b
stealth-ultd <47171697+stealth-ultd@users.noreply.github.com>
2021-12-08 23:54:53 +0100

    Update PowerPilot.ino

--- Regulator/PowerPilot.ino
+++ Regulator/PowerPilot.ino
@@ -1,136 +1,103 @@
 
+// uncomment line 73-75 for MQTT_ON
+
 void pilotTriacPeriod(float p) {
   Triac::setPeriod(p);
 }
 
 void pilotSetup() {
   Triac::setup(ZC_EI_PIN, TRIAC_PIN);
+  pilotTriacPeriod(0);
 }
 
-const int MIN_POWER = 300;
+const int MIN_POWER = 35; // W, lowest absolute value for Triac activation
+const int MIN_START_POWER = 50; // W, lowest treshold for PowerPilot start
+const int INSOL_BUFFER = 10; // W, safety net on low insol levels// 30
+const int BYPASS_POWER = 700; // when the TA-heater goes in bypass mode (relay) // 700
+const int BYPASS_MIN_START_POWER = BYPASS_POWER + 20;
+
+float tresholdMAvg; // value for moving average
+long tresholdReport; // delay in updating tresholdAv
+unsigned long nTRS; // number of samples
+
 
 void pilotLoop() {
 
-  const int PUMP_POWER = 40;
-  const byte MONITORING_UNTIL_SOC = 85; // %
-  const byte BYPASS_BUFFER_SOC = 95; // %
-  const int CONSUMPTION_POWER_LIMIT = 3900; // W
-  const int BATTERY_MAX_CHARGING_POWER = 4000; // W
-  const byte TOP_OSCILLATION_SOC = 97; // %
-  const int TOP_OSCILLATION_DISCHARGE_LIMIT = -600; // W
-  const int TOP_OSCILLATION_COUNTERMEASURE = -20; // W
-  const int LEFT_FOR_BATTERY = -200; // W used if heating bellow BYPASS_BUFFER_SOC
-  const int MIN_START_POWER = 500; // W
-  const int BYPASS_MIN_START_POWER = BYPASS_POWER + 100;
-  const int HEATING_PRIORITY_START_POWER = BYPASS_POWER + 250;
-  const byte WAIT_FOR_IT_COUNT = 3;
+// sum available power
+   short availablePower = heatingPower + meterPower + (insolPowerAvg * 0.95) - inverterAC - INSOL_BUFFER; // 0.95 to be on safe side
 
-  static byte waitForItCounter = 0; // to not react on short spikes
-
-  switch (state) {
-    case RegulatorState::MONITORING:
-      break;
-    case RegulatorState::REGULATING:
-      if (!mainRelayOn) {
-        state = RegulatorState::MONITORING;
-      }
-      break;
-    default:
-      return;
+// PV Throttled "overdraw" protection
+// checks if heatingPower pushes meterPower into minus on underperfoming PV output (compared to PV reference panel, i.e. snow)
+    tresholdAveraging(); // we have to be patient as the inverter needs time to ramp up to the power consumption demand
+    if (((float) insolPowerAvg / (inverterAC + 1)  >= 1.20) && tresholdAvg <= -50) { // in case PV underperforms, don't draw power
+    availablePower = 0;
+    pilotThrottled = true; // status to display
+  } else {
+    pilotThrottled = false;
   }
 
-  // handle heating strategy plan
-  if (hourNow >= 12 && (powerPilotPlan == HEATING_PRIORITY_AM || powerPilotPlan == HEATING_DISABLED_AM)) {
-    powerPilotPlan = BATTERY_PRIORITY;
-  }
-  if (powerPilotPlan == HEATING_DISABLED || powerPilotPlan == HEATING_DISABLED_AM) {
-    if (heatingPower > 0) {
-      heatingPower = 0;
-      powerPilotRaw = 0;
+// set pilot strategy
+
+// case: MQTT set
+  if (setPower > 0) { // MQTT setting overrules regulation
+      availablePower = setPower; 
+      state = RegulatorState::MANUAL_RUN;
     }
-    return;
+// case: NightCharge 
+  if (nightCall) { // check if nightcharge is on    
+  chargeSetLevel = (0.67 * MAX_ACCUMULATE) + (0.33* MAX_ACCUMULATE * chargeSetRatio / 100); // accumulation at 62,5%, 75%, 87,5%, or 100% of MAX
+    if (nightChargeHours() && statsAccumulatedPowerToday() <= chargeSetLevel) {  
+    availablePower = MAX_POWER;
+    state = RegulatorState::ACCUMULATE;
+    }
   }
-
-  int pvChP = (pvChargingPower > 0) ? 0 : pvChargingPower; // as default take only negative charge (discharge)
-  if (!pvBattCalib) { // at battery calibration. battery takes what is needed. surplus power can occur
-
-  // check state of charge
-  // * the Symo regulates the distribution of energy while the battery is charging.
-  // usually we don't want to interfere with that (unless heating has priority),
-  // but we must enter the regulation algorithm if heating is on.
-  // * if the production is larger then the battery can use for charging, surplus can occur
-  if (powerPilotPlan != HEATING_PRIORITY_AM && pvSOC < MONITORING_UNTIL_SOC
-      && heatingPower == 0 && pvChargingPower < BATTERY_MAX_CHARGING_POWER)
+    
+// case: suspend on low startup power
+  if (!MQTT_ON && heatingPower == 0 && availablePower < MIN_START_POWER) { // interferes with MQTT setPower = 0
     return;
-
-  // in bypass mode, we can use battery as buffer to ignore short PV shadows or small concurrent consumption
-  if (bypassRelayOn && pvSOC >= BYPASS_BUFFER_SOC && inverterAC < CONSUMPTION_POWER_LIMIT)
-    return;
-
-  // battery charge/discharge control
-  if ((powerPilotPlan == HEATING_PRIORITY_AM || pvSOC >= BYPASS_BUFFER_SOC)
-      && (heatingPower + pvChargingPower) > (mainRelayOn ? BYPASS_POWER : HEATING_PRIORITY_START_POWER)
-      && (inverterAC - heatingPower + BYPASS_POWER) < CONSUMPTION_POWER_LIMIT) {
-    pvChP = pvChargingPower; // take this big charging as available (not everything will be used and/or the battery can charge later)
-  } else if (pvSOC < BYPASS_BUFFER_SOC && pvChargingPower < BATTERY_MAX_CHARGING_POWER) {
-    pvChP += LEFT_FOR_BATTERY; // retreat in steps, if the battery takes it
-  } else if (pvSOC > TOP_OSCILLATION_SOC && pvChP < TOP_OSCILLATION_COUNTERMEASURE
-      && (pvChP + meterPower) > TOP_OSCILLATION_DISCHARGE_LIMIT) { // tolerated discharge
-    pvChP = TOP_OSCILLATION_COUNTERMEASURE; // almost ignore discharge. full battery can do a little discharging
-  }
-
-  } // close if (!pvBattCalib) {
-
-  // sum available power
-  short availablePower = heatingPower + meterPower + pvChP - (mainRelayOn ? 0 : PUMP_POWER);
-  if (heatingPower == 0 && availablePower < MIN_START_POWER) {
-    waitForItCounter = 0;
-    return;
-  }
+    }
+// case: Bypass relay active
   if (bypassRelayOn && availablePower > BYPASS_POWER)
     return;
-  if (availablePower < MIN_POWER) {
-    heatingPower = 0;
+// case: switch to MONITORING on low power
+  if (availablePower < MIN_POWER) { // is power less than starting up treshold? 
+    heatingPower = 0; // check this
     powerPilotRaw = 0;
-    waitForItCounter = 0;
-    return;
+    availablePower = 0;
+    state = RegulatorState::MONITORING;
+    
+      if (bypassRelayOn) {
+        waitZeroCrossing();
+        // the actual switching is after 10 ms on next ZC
+        digitalWrite(BYPASS_RELAY_PIN, LOW);
+        bypassRelayOn = false;
+        }
+ 
+    if (!MQTT_ON) { 
+//    return; // commented to prevent power being "stuck" with MQTT setPower = 0 <<<<<<<<<<<<<< !!
+    }
+  
   }
+// case: regulating
+if (availablePower >= MIN_START_POWER && setPower == 0 && !nightChargeHours()) { // – are we regulating?
+    state = RegulatorState::REGULATING;    
+    }  
 
-  // bypass the power regulator module for max power
-  boolean bypass = availablePower > (bypassRelayOn ? BYPASS_POWER : BYPASS_MIN_START_POWER);
+// bypass the triac for max power
+boolean bypass = availablePower > (bypassRelayOn ? BYPASS_POWER : BYPASS_MIN_START_POWER);
 
-  // not switch relays for short spikes
-  if ((!mainRelayOn || (!bypassRelayOn && bypass)) && waitForItCounter < WAIT_FOR_IT_COUNT) {
-    waitForItCounter++;
-    if (!mainRelayOn)
-      return;
-    bypass = false;
-  } else {
-  waitForItCounter = 0;
-  }
-
-  if (!turnMainRelayOn())
-    return;
-  state = RegulatorState::REGULATING;
-
-  // set heating power
+// set heating power
   float r = bypass ? 0 : power2TriacPeriod(availablePower);
   pilotTriacPeriod(r);
-  powerPilotRaw = r * 1000; // to log
+  powerPilotRaw = r * 1000; // to log 
 
-  if (bypass != bypassRelayOn) {
+if (bypass != bypassRelayOn) {
     waitZeroCrossing();
     digitalWrite(BYPASS_RELAY_PIN, bypass);
     bypassRelayOn = bypass;
   }
-  heatingPower = bypass ? BYPASS_POWER : (availablePower > MAX_POWER) ? MAX_POWER : availablePower;
-}
 
-void powerPilotSetPlan(byte plan) {
-  if (powerPilotPlan == plan)
-    return;
-  eventsWrite(POWERPILOT_PLAN_EVENT, plan, powerPilotPlan);
-  powerPilotPlan = plan;
+heatingPower = bypass ? BYPASS_POWER : (availablePower > MAX_POWER) ? MAX_POWER : availablePower;
 }
 
 void powerPilotStop() {
@@ -139,13 +106,29 @@ void powerPilotStop() {
 }
 
 float power2TriacPeriod(int power) {
-  const float POWER2PERIOD_SHIFT = 0.08;
-  const float POWER2PERIOD_KOEF = 0.5;
+  const float POWER2PERIOD_SHIFT = 0.027 ; // 0.03
+  const float POWER2PERIOD_KOEF = 0.61; // 0.605
 
   if (power < MIN_POWER)
     return 0.0;
   if (power >= MAX_POWER)
     return 0.95;
   float ratio = (float) power / MAX_POWER;
-  return POWER2PERIOD_SHIFT + POWER2PERIOD_KOEF * asin(sqrt(ratio));
+  float linevoltagefactor = (float) 220 / voltage; // line voltage correction
+  
+  
+ return POWER2PERIOD_SHIFT + POWER2PERIOD_KOEF * asin(sqrt(ratio)) * linevoltagefactor; // * linevoltage; 
+}
+
+void tresholdAveraging() { // floating average for PV "overdraw" protection check
+  int treshold = heatingPower + meterPower;
+  
+  nTRS++; // update sample count
+  tresholdMAvg += (treshold - tresholdMAvg) / nTRS; // calculate moving average
+    
+    if (loopStartMillis - tresholdReport > 10 * 1000) { // 10 secs polling
+    tresholdAvg = tresholdMAvg; // retreive result after polling time
+    tresholdReport = loopStartMillis;
+    nTRS = 0; // to start a new averaging period, set number of samples (n) to zero
+    }
 }
