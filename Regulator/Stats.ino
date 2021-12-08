c5626e9c5fd7aa6e15fc1ab0391829177f72ac76
stealth-ultd <47171697+stealth-ultd@users.noreply.github.com>
2021-12-08 23:56:45 +0100

    Update Stats.ino

--- Regulator/Stats.ino
+++ Regulator/Stats.ino
@@ -1,47 +1,34 @@
+// ** Compiles the Stats and saves to SD card (FAT 32) **
 
-#ifdef NO_EEPROM
 const char* STATS_FILENAME = "STATS.DAT";
-#else
-const int STATS_EEPROM_ADDR = 256;
-#endif
 
-struct {
-  time_t timestamp = 0;
-
+  struct {
+  time_t timestamp = 0; 
   Stats day;
   Stats month;
-  Stats dayExtHeater;
-  Stats monthExtHeater;
+  Stats year;
+  Stats dayAccumulate;
+  Stats monthAccumulate;
+  Stats yearAccumulate;
   Stats dayManualRun;
   Stats monthManualRun;
-} statsData;
+  Stats yearManualRun;
+  } statsData;
 
 unsigned long statsMilliwats = 0;
 unsigned long statsMilliwatMilis = 0;
+bool statsAccumulateFlag;
 bool statsManualRunFlag;
+bool statsRegulateFlag;
 unsigned long statsSaveTimer = 0;
-bool updateBlynk = true;
 
-unsigned long statsExtHeaterStartMillis = 0;
-unsigned statsExtHeaterSeconds = 0;
 
 void statsSetup() {
-#ifdef NO_EEPROM
   File file = FS.open(STATS_FILENAME, FILE_READ);
   if (file) {
     file.readBytes((byte*) &statsData, sizeof(statsData));
     file.close();
   }
-#else
-  EEPROM.get(STATS_EEPROM_ADDR, statsData);
-  statsData.dayManualRun.heatingTime = 0;
-  statsData.dayManualRun.consumedPower = 0;
-  statsData.monthManualRun.heatingTime = 0;
-  statsData.monthManualRun.consumedPower = 0;
-  if ((unsigned long) statsData.dayManualRun.heatingTime == 0xFFFFFFFF) { // empty EEPROM
-    memset(&statsData, 0, sizeof(statsData));
-  }
-#endif
 }
 
 void statsLoop() {
@@ -51,48 +38,42 @@ void statsLoop() {
   static unsigned long lastPowerChangeMillis = 0;
   static int lastPower = 0;
 
-  if (statsExtHeaterStartMillis == 0) { // not counting
-    if (extHeaterIsOn) {
-      statsExtHeaterStartMillis = loopStartMillis; // start counting
-    }
-  } else if (!extHeaterIsOn) { // heater stopped
-    statsExtHeaterSeconds += (loopStartMillis - statsExtHeaterStartMillis) / 1000;
-    statsExtHeaterStartMillis = 0;
-  }
-
   if (lastPowerChangeMillis == 0) { // not counting
-    if (!mainRelayOn)
-      return;
-    lastPowerChangeMillis = loopStartMillis; // relay is on, start counting
+    lastPowerChangeMillis = loopStartMillis; // start counting
     statsSaveTimer = loopStartMillis;
-    statsManualRunFlag = (state == RegulatorState::MANUAL_RUN || manualRunRequest);
   }
   time_t t = statsData.timestamp;
   if (day(now()) != day(t)) {
-#ifdef FS
+
     char buff[32];
     sprintf(buff, "DATA%d.CSV", year(t));
     File file = FS.open(buff, FILE_WRITE);
     if (file) {
-      sprintf(buff, "%d-%02d-%02d;%ld;%ld;%ld;%ld;%ld;%ld", year(t), month(t), day(t), //
-          statsData.day.heatingTime, statsData.day.consumedPower, //
-          statsData.dayExtHeater.heatingTime, statsData.dayExtHeater.consumedPower, //
-          statsData.dayManualRun.heatingTime, statsData.dayManualRun.consumedPower);
+      sprintf(buff, "%d-%02d-%02d;%04d;%04d;%04d", // "%d-%02d-%02d;%ld;%ld;%ld"
+          year(t), month(t), day(t),
+          statsData.day.consumedPower,
+          statsData.dayAccumulate.consumedPower,
+          statsData.dayManualRun.consumedPower);
       file.println(buff);
       file.close();
     }
-#endif
+
     if (month(now()) != month(statsData.timestamp)) {
-      memset(&statsData, 0, sizeof(statsData));
-    } else {
-      statsData.day.heatingTime = 0;
+//      memset(&statsData, 0, sizeof(statsData.month.consumedPower));
+//      memset(&statsData, 0, sizeof(statsData.monthAccumulate.consumedPower));
+//      memset(&statsData, 0, sizeof(statsData.monthManualRun.consumedPower));
+      statsData.month.consumedPower = 0;
+      statsData.monthAccumulate.consumedPower = 0;
+      statsData.monthManualRun.consumedPower = 0;  
+    }
+    if (year(now()) != year(statsData.timestamp)) { // this section is new
+      memset(&statsData, 0, sizeof(statsData));   
+    }
+    else {
       statsData.day.consumedPower = 0;
-      statsData.dayExtHeater.heatingTime = 0;
-      statsData.dayExtHeater.consumedPower = 0;
-      statsData.dayManualRun.heatingTime = 0;
+      statsData.dayAccumulate.consumedPower = 0;
       statsData.dayManualRun.consumedPower = 0;
     }
-    msg.print(" stats_reset");
     statsData.timestamp = now();
   }
 
@@ -104,26 +85,22 @@ void statsLoop() {
       statsMilliwatMilis += interval;
       if (power == 0 || statsMilliwatMilis > STATS_MILLIWATS_INTERVAL) {
         statsAddMilliwats();
-        statsManualRunFlag = (state == RegulatorState::MANUAL_RUN);
       }
     }
     lastPower = power;
     lastPowerChangeMillis = loopStartMillis;
   }
 
-  if (!mainRelayOn) {
-    lastPowerChangeMillis = 0; // stop counting
-    statsSave();
-  }
-
   if (loopStartMillis - statsSaveTimer > STATS_SAVE_INTERVAL) {
     statsSave();
   }
+
 }
 
 int statsEvalCurrentPower() {
 
   switch (state) {
+    case RegulatorState::ACCUMULATE:
     case RegulatorState::MANUAL_RUN:
     case RegulatorState::REGULATING:
       return elsensPower;
@@ -137,113 +114,85 @@ void statsAddMilliwats() {
   int consumedPower = round((float) statsMilliwats / 1000); // watt
   statsMilliwatMilis = 0;
   statsMilliwats = 0;
-  if (statsManualRunFlag) {
-    statsData.dayManualRun.heatingTime += heatingTime;
-    statsData.dayManualRun.consumedPower += consumedPower;
-    statsData.monthManualRun.heatingTime += heatingTime;
-    statsData.monthManualRun.consumedPower += consumedPower;
-  } else {
-    statsData.day.heatingTime += heatingTime;
-    statsData.day.consumedPower += consumedPower;
-    statsData.month.heatingTime += heatingTime;
-    statsData.month.consumedPower += consumedPower;
-  }
+  
+  statsAccumulateFlag = (state == RegulatorState::ACCUMULATE);
+  statsManualRunFlag = (state == RegulatorState::MANUAL_RUN);
+  statsRegulateFlag = (state == RegulatorState::REGULATING || state == RegulatorState::MONITORING);
 
-  if (statsExtHeaterSeconds != 0) {
-    heatingTime =  round((float) statsExtHeaterSeconds / 60); // minutes
-    consumedPower = round(statsExtHeaterSeconds * ((float) EXT_HEATER_POWER / 3600)); // watt
-    statsExtHeaterSeconds = 0;
-    statsData.dayExtHeater.heatingTime += heatingTime;
-    statsData.dayExtHeater.consumedPower += consumedPower;
-    statsData.monthExtHeater.heatingTime += heatingTime;
-    statsData.monthExtHeater.consumedPower += consumedPower;
+        
+ if (statsAccumulateFlag) {
+    statsData.dayAccumulate.consumedPower += consumedPower;
+    statsData.monthAccumulate.consumedPower += consumedPower;
+    statsData.yearAccumulate.consumedPower += consumedPower;
+    }
+  if (statsManualRunFlag) {
+    statsData.dayManualRun.consumedPower += consumedPower;
+    statsData.monthManualRun.consumedPower += consumedPower;
+    statsData.yearManualRun.consumedPower += consumedPower;
+    }
+  if (statsRegulateFlag) {
+    statsData.day.consumedPower += consumedPower;
+    statsData.month.consumedPower += consumedPower;
+    statsData.year.consumedPower += consumedPower;
+    
   }
-  updateBlynk = true;
 }
 
 void statsSave() {
   if (!statsMilliwatMilis)
     return;
   statsAddMilliwats();
-#ifdef NO_EEPROM
+
   File file = FS.open(STATS_FILENAME, FILE_NEW);
   if (file) {
     file.write((byte*) &statsData, sizeof(statsData));
     file.close();
   }
-#else
-  EEPROM.put(STATS_EEPROM_ADDR, statsData);
-#endif
+
   eventsWrite(STATS_SAVE_EVENT, (loopStartMillis - statsSaveTimer) / 60000, 0);
   statsSaveTimer = loopStartMillis;
-  msg.print(" stats_saved");
 }
 
-int statsConsumedPowerToday() {
+int statsRegulatedPowerToday() { // this is used by display and webserver
   if (day(now()) != day(statsData.timestamp))
     return 0;
-  return statsData.day.consumedPower + statsData.dayExtHeater.consumedPower + statsData.dayManualRun.consumedPower;
+  return statsData.day.consumedPower; // Wh
 }
 
-void statsPrint(FormattedPrint& out) {
-  statsPrint(out, "Day heating", statsData.day);
-  statsPrint(out, "Month heating", statsData.month);
-  statsPrint(out, "Day external", statsData.dayExtHeater);
-  statsPrint(out, "Month external", statsData.monthExtHeater);
-  statsPrint(out, "Day manual-run", statsData.dayManualRun);
-  statsPrint(out, "Month manual-run", statsData.monthManualRun);
+int statsAccumulatedPowerToday() { // this is used by display and webserver
+  if (day(now()) != day(statsData.timestamp))
+    return 0;
+  return statsData.dayAccumulate.consumedPower; // Wh
 }
 
-void statsPrint(FormattedPrint& out, const char *label, Stats &stats) {
-  int h = stats.heatingTime / 60;
-  int m = stats.heatingTime - h * 60;
-  out.printf(F("%s time: %d:%02d\r\n"), label, h, m);
-  out.printf(F("%s power: %ld W\r\n"), label, stats.consumedPower);
+int statsManualPowerToday() { // this is used by display and webserver
+  if (day(now()) != day(statsData.timestamp))
+    return 0;
+  return statsData.dayManualRun.consumedPower; // Wh
 }
 
 void statsPrintJson(FormattedPrint& out) {
   out.printf(F("{\"timestamp\":%lu,"
-      "\"dayHeatingTime\":%ld,\"dayConsumedPower\":%ld,"
-      "\"monthHeatingTime\":%ld,\"monthConsumedPower\":%ld,"
-      "\"dayExtHeatingTime\":%ld,\"dayExtConsumedPower\":%ld,"
-      "\"monthExtHeatingTime\":%ld,\"monthExtConsumedPower\":%ld,"
-      "\"dayManualRunTime\":%ld,\"dayManualRunPower\":%ld,"
-      "\"monthManualRunTime\":%ld,\"monthManualRunPower\":%ld"),
+      "\"emptyline\":%ld,"
+      "\"dayConsumedPower\":%ld,"
+      "\"monthConsumedPower\":%ld,"
+      "\"yearConsumedPower\":%ld,"
+      "\"dayAccumulatePower\":%ld,"
+      "\"monthAccumulatePower\":%ld,"
+      "\"yearAccumulatePower\":%ld,"    
+      "\"dayManualRunPower\":%ld,"
+      "\"monthManualRunPower\":%ld,"
+      "\"yearManualRunPower\":%ld"),
       statsData.timestamp,
-      statsData.day.heatingTime, statsData.day.consumedPower,
-      statsData.month.heatingTime, statsData.month.consumedPower,
-      statsData.dayExtHeater.heatingTime, statsData.dayExtHeater.consumedPower,
-      statsData.monthExtHeater.heatingTime, statsData.monthExtHeater.consumedPower,
-      statsData.dayManualRun.heatingTime, statsData.dayManualRun.consumedPower,
-      statsData.monthManualRun.heatingTime, statsData.monthManualRun.consumedPower);
-#ifdef FS
+      statsData.day.consumedPower,
+      statsData.month.consumedPower,
+      statsData.year.consumedPower,
+      statsData.dayAccumulate.consumedPower,
+      statsData.monthAccumulate.consumedPower,
+      statsData.yearAccumulate.consumedPower,    
+      statsData.dayManualRun.consumedPower,
+      statsData.monthManualRun.consumedPower,
+      statsData.yearManualRun.consumedPower);
    out.printf(",\"fn\":\"DATA%d.CSV\"", year());
-#endif
    out.print('}');
-}
-
-void statsBlynk() {
-  if (!updateBlynk)
-    return;
-  updateBlynk = false;
-  Blynk.virtualWrite(STATS_TABLE_WIDGET, "clr");
-  unsigned long t = statsData.timestamp;
-  char buff[32];
-  sprintf_P(buff, PSTR("%d-%02d-%02d"), year(t), month(t), day(t));
-  Blynk.virtualWrite(STATS_TABLE_WIDGET, "add", 0, buff, "W");
-  byte i = 1;
-  statsBlynkRow(i++, "Day heating", statsData.day);
-  statsBlynkRow(i++, "Month heating", statsData.month);
-  statsBlynkRow(i++, "Day external", statsData.dayExtHeater);
-  statsBlynkRow(i++, "Month external", statsData.monthExtHeater);
-  statsBlynkRow(i++, "Day manual-run", statsData.dayManualRun);
-  statsBlynkRow(i++, "Month manual-run", statsData.monthManualRun);
-}
-
-void statsBlynkRow(byte i, const char *label, Stats &stats) {
-  int h = stats.heatingTime / 60;
-  int m = stats.heatingTime - h * 60;
-  char buff[32];
-  snprintf_P(buff, sizeof(buff), PSTR("%s %d:%02d"), label, h, m);
-  Blynk.virtualWrite(STATS_TABLE_WIDGET, "add", i, buff, stats.consumedPower);
 }
